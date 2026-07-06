ea00f6192614ae4aa8a47a31e45d398a7009a17b
niyachelowick <gary_loc.kid@hotmail.com>
2026-07-06 17:55:17 -0600

    First commit from nvim
    Se migró todo el proyecto a un ecosistema de desarrollo basado en vim,
    por ello, se reescrivieron algunas lineas que tenían el salto de linea
    de windows, lo que hacía que el LSP arrojara errores.

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,116 @@
-/*This "main.cpp" file is divided by sections, each section contains an example about
-the usage of the classes of this little library.
-To run each example, just uncomment the content of the section you wanna test. :3 */
+/*This "main.cpp" file is divided by sections, each section contains an example
+about the usage of the classes of this little library. To run each example, just
+uncomment the content of the section you wanna test. :3 */
 
 #include <SFML/Graphics.hpp>
-#include <iostream>
+// #include <iostream>
 #include "bullet.hpp"
 
 sf::CircleShape _bullet(8, 16);
 sf::Vector2f origen;
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1200, 608}), "CMake SFML Project");
-    window.setFramerateLimit(30);
-    float oriX = window.getSize().x / 2;
-    float oriY = window.getSize().y / 2;
-    origen = {oriX, oriY};
-    /************************** Here Starts the parabolic shoot example ****************/
-    // //declaring and instansing a physicVector:
-    // physicVector *Velocity,*gravity,*velCaida;
-    // // just a SFML's Circle shape to manipulate it.
-    // _bullet.setOrigin({8,8});
-    // //let's instance the vectors, they recibe a magnitude, a direction, and optionaly, origin.
-    // Velocity=new physicVector(10,315,origen);
-    // gravity=new physicVector(0.1,90);
-    // velCaida=gravity;// You can even igualate a vector to another vector
-    // velCaida->setMagnitude(0);//and modify its atributes by setters.
-    // _bullet.setPosition({0,304});
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({1200, 608}), "CMake SFML Project");
+  window.setFramerateLimit(30);
+  float oriX = window.getSize().x / 2.0;
+  float oriY = window.getSize().y / 2.0;
+  origen = {oriX, oriY};
+  /************************** Here Starts the parabolic shoot example
+   * ****************/
+  // //declaring and instansing a physicVector:
+  // physicVector *Velocity,*gravity,*velCaida;
+  // // just a SFML's Circle shape to manipulate it.
+  // _bullet.setOrigin({8,8});
+  // //let's instance the vectors, they recibe a magnitude, a direction, and
+  // optionaly, origin. Velocity=new physicVector(10,315,origen); gravity=new
+  // physicVector(0.1,90); velCaida=gravity;// You can even igualate a vector to
+  // another vector velCaida->setMagnitude(0);//and modify its atributes by
+  // setters. _bullet.setPosition({0,304});
 
-    // //the "while" bellow, is something from SFML,
-    // //you can see:https://www.sfml-dev.org/tutorials/3.0 for more info.
-    // while (window.isOpen())
-    // {
-    //     while (const std::optional event = window.pollEvent())
-    //     {
-    //         if (event->is<sf::Event::Closed>())
-    //         {
-    //             window.close();
-    //         }
-    //     }
+  // //the "while" bellow, is something from SFML,
+  // //you can see:https://www.sfml-dev.org/tutorials/3.0 for more info.
+  // while (window.isOpen())
+  // {
+  //     while (const std::optional event = window.pollEvent())
+  //     {
+  //         if (event->is<sf::Event::Closed>())
+  //         {
+  //             window.close();
+  //         }
+  //     }
 
-    //     gravity->integrate(*velCaida);//this will acumulate the magnitud by iteration.
-    //     //Then we can add the fallingVelocity to our object velocity vector:
-    //     Velocity->add(*velCaida);
-    //     std::cout<<Velocity->getMagitude()<<"\t"<<Velocity->getDirection()<<std::endl;
-    //     //and we can move a SFML shape object by its "move" method
-    //     _bullet.move(Velocity->getComponents());
+  //     gravity->integrate(*velCaida);//this will acumulate the magnitud by
+  //     iteration.
+  //     //Then we can add the fallingVelocity to our object velocity vector:
+  //     Velocity->add(*velCaida);
+  //     std::cout<<Velocity->getMagitude()<<"\t"<<Velocity->getDirection()<<std::endl;
+  //     //and we can move a SFML shape object by its "move" method
+  //     _bullet.move(Velocity->getComponents());
 
-    //     window.clear();
-    //     window.draw(_bullet);//just draw the object to the renderWindow.
-    //     window.display();
-    //     if(_bullet.getPosition().y>=window.getSize().y)break;;
-    // }
-    /********************** Here ends the parabolic shoot expamle ****************************/
+  //     window.clear();
+  //     window.draw(_bullet);//just draw the object to the renderWindow.
+  //     window.display();
+  //     if(_bullet.getPosition().y>=window.getSize().y)break;;
+  // }
+  /********************** Here ends the parabolic shoot exapmle
+   * ****************************/
 
-    /****************Here starts Danmaku pincipies example*****************/
-    const int quantity = 8, tamanioFila = 10;
-    bool espaciar = true;
-    bullet *bu1[quantity][tamanioFila];
-    // first, we use a for loop to initialice the bullet array
-    for (uint8_t i = 0; i < quantity; i++)
-    {
-        float degs = i * (360.0 / quantity); // The class uses by default sexagecimal degrees
-        // we can intiate the bullet by the same parameters of a physicVector
-        for (uint8_t j = 0; j < 10; j++)
-        {
-            bu1[i][j] = new bullet(7, degs, origen);
-            // if you wanna curve the trayectories, you can add an angular modifier
-            // the angular modifier will change orientation of the velocity vector by a delta
-            // bu1[i]->angularModifier(-0.05);
-            bu1[i][j]->angularModifier(3);
-        }
+  /****************Here starts Danmaku principles example*****************/
+  const int quantity = 8, tamanioFila = 20;
+  bool espaciar = true;
+  bullet *bu1[quantity][tamanioFila];
+  // first, we use a for loop to initialize the bullet array
+  for (uint8_t i = 0; i < quantity; i++) {
+    float degs =
+        i * (360.0 / quantity); // The class uses by default sexagesimal degrees
+    // we can initiate the bullet by the same parameters of a physicVector
+    for (uint8_t j = 0; j < tamanioFila; j++) {
+      bu1[i][j] = new bullet(7, degs, origen);
+      // if you wanna curve the trajectories, you can add an angular modifier
+      // the angular modifier will change orientation of the velocity vector by
+      // a delta bu1[i]->angularModifier(-0.05);
+      bu1[i][j]->angularModifier(3);
     }
-    // esto actualiza los eventos del juego en función del tiempo
-    sf::Clock reloj;
-    sf::Time trigger;
-    sf::Time separation = sf::milliseconds(250);
-    uint8_t shootedBullets = 0;
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
-        if (reloj.getElapsedTime() >= separation)
-        {
-            trigger = reloj.restart();
-            if(shootedBullets<tamanioFila)
-                shootedBullets++;
-            
-        }
-        for (uint8_t i = 0; i < quantity; i++)
-        {
-            for (uint8_t j = 0; j <shootedBullets; j++)
-                bu1[i][j]->move(); // tell the bullet increase its position.
-        }
-        // you can comment the "window.clear()" to see how the patter will apear
-        // when object pooling get implemented.
-        window.clear();
-        for (uint8_t i = 0; i < quantity; i++)
-        {
-            // indicate to the bullet in wich render target will apear.
-            for (uint8_t j = 0; j < shootedBullets; j++)
-                bu1[i][j]->drawTo(window);
-        }
-        espaciar = !espaciar;
-        window.display();
+  }
+  // esto actualiza los eventos del juego en función del tiempo
+  sf::Clock reloj;
+  sf::Time trigger;
+  sf::Time separation = sf::milliseconds(100);
+  uint8_t shootedBullets = 0;
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
-    // Liberar la memoria al finalizar el programa
+    if (reloj.getElapsedTime() >= separation) {
+      trigger = reloj.restart();
+      if (shootedBullets < tamanioFila)
+        shootedBullets++;
+    }
     for (uint8_t i = 0; i < quantity; i++) {
-        for (uint8_t j = 0; j < tamanioFila; j++) {
-            delete bu1[i][j];
-        }
+      for (uint8_t j = 0; j < shootedBullets; j++)
+        bu1[i][j]->move(); // tell the bullet increase its position.
     }
+    // you can comment the "window.clear()" to see how the patter will apear
+    // when object pooling get implemented.
+    window.clear();
+    for (uint8_t i = 0; i < quantity; i++) {
+      // indicate to the bullet in which render target will apear.
+      for (uint8_t j = 0; j < shootedBullets; j++)
+        bu1[i][j]->drawTo(window);
+    }
+    espaciar = !espaciar;
+    window.display();
+  }
+  // Liberar la memoria al finalizar el programa
+  for (uint8_t i = 0; i < quantity; i++) {
+    for (uint8_t j = 0; j < tamanioFila; j++) {
+      delete bu1[i][j];
+    }
+  }
 
-    return 0;
-    /****************Here ends Danmaku pincipies example*****************/
+  return 0;
+  /****************Here ends Danmaku principles example*****************/
 }
